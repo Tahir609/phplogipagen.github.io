cf2c4d04edf3458c1b713338895394cf237d6f0d
Tahir <tahirnaeem733@gmail.com>
2022-04-21 02:57:47 -0700

    php login page

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        Wellcome <?php echo $_POST["name"]; ?><br>
+        Your Email Address is : <?php echo $_POST["email"]; ?><br>
+        Your Password is :<?php echo $_POST["password"]?><br>
+        <a href="login.html">Go to Back</a> 
+    </body>
+</hmtl>
